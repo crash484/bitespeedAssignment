931ee3a3155c0dca237291d6b8f4f42242a1a37c
Shashwat <awesomedude019@gmail.com>
2026-02-26 23:03:10 +0530

    updated routes

--- routes/identify.ts
+++ routes/identify.ts
@@ -5,10 +5,30 @@ import { IdentifyRequest } from "../types/Contact";
 const router = Router();
 
 router.post("/identify", async(req:Request,res:Response)=>{
-    const {email,phoneNumber} = req.body
+    const {email,phoneNumber} = req.body as IdentifyRequest
 
     //validate
     if( !email && !phoneNumber){
-        res.status(400).json({error:"email or phone number is missing"})
+        res.status(400).json({error:"email or phone number is missing"});
+        return;
     }
+
+    //check
+    if (email !== undefined && typeof email !== "string") {
+        res.status(400).json({ error: "'email' must be a string." });
+        return;
+    }
+
+    if (phoneNumber !== undefined && typeof phoneNumber !== "string") {
+        res.status(400).json({ error: "'phoneNumber' must be a string." });
+        return;
+    }
+
+     try {
+        const result = await identify({ email, phoneNumber });
+        res.status(200).json(result);
+    } catch (err) {
+        console.error("[/identify] Error:", err);
+        res.status(500).json({ error: "Internal server error." });
+  }
 })
